3175fef6d580a40a2c32e9c54ea6ba2927c11864
Adi Amoyal <adi.amoyal02@gmail.com>
2023-04-07 12:01:33 +0300

    Final changes

--- CryptoApp/CryptoAppApp.swift
+++ CryptoApp/CryptoAppApp.swift
@@ -23,6 +23,7 @@ struct CryptoAppApp: App {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .navigationViewStyle(StackNavigationViewStyle())
             .environmentObject(vm)
         }
     }
